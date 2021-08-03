062f9e49e269613b5fd8dad6bf0b9185ddf6fbab
UurZer <46075903+UurZer@users.noreply.github.com>
2021-08-03 15:07:47 +0300

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { CarComponent } from './component/car/car.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",pathMatch:"full", component:CarComponent},
+  {path:"cars",pathMatch:"full", component:CarComponent},
+  {path:"cars/brand/:brandid",component:CarComponent},
+  {path:"cars/color/:colorid",component:CarComponent},
+  {path:"cars/cardetail/:carid",component:CarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
